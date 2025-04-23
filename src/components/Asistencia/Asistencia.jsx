abcfde9e2ec69aee1c5e90fad760068d1695f6d7
Agustin Scaramello <agusscaramello@gmail.com>
2025-04-23 20:33:34 -0300

    fix/botonesAsistencia

--- src/components/Asistencia/Asistencia.jsx
+++ src/components/Asistencia/Asistencia.jsx
@@ -14,11 +14,12 @@ export const Asistencia = ({ audioRef }) => {
 	}
 
 	const evento = {
-		text: "👑¡FIESTA XV DE EVELIN!👑",
-		details: "¡Veni a pasar una noche increible, con ganas de bailar y disfrutar, no te la podes perder! ✨",
-		location: "-34.74226566628524, -58.27333427639575",
-		startDate: "20250502", // Fecha en formato UTC (AñoMesDíaTHoraMinutoSegundoZ)
-		endDate: "20250503",
+		text: "👑¡FIESTA XV DE EVELIN!👑 (NUEVA FECHA)",
+		details:
+			"¡Veni a pasar una noche increible, con ganas de bailar y disfrutar, no te la podes perder! ✨ A las 22:00hs. en Mitre 674, Florencio Varela",
+		location: "-34.807818734043096, -58.2675428320896",
+		startDate: "20250517", // Fecha en formato UTC (AñoMesDíaTHoraMinutoSegundoZ)
+		endDate: "20250518",
 	}
 
 	const generarURLGoogleCalendar = () => {
@@ -73,7 +74,7 @@ export const Asistencia = ({ audioRef }) => {
 			<div className="containerBotonesAsistencia animation-container">
 				<a
 					className="containerbtnAsistencia animate-item btn-asistire"
-					href="https://wa.me/5491138978533?text=%C2%A1Hola%2C%20Eve!%0A%0AQuer%C3%ADa%20decirte%20que%20estoy%20s%C3%BAper%20emocionad%40%20de%20celebrar%20este%20d%C3%ADa%20tan%20especial%20contigo.%20%C2%A1Confirmo%20mi%20asistencia%20a%20tu%20fiesta%20de%2015!%0A%0A%C2%A1Seguro%20va%20a%20ser%20una%20noche%20m%C3%A1gica%20llena%20de%20alegr%C3%ADa%2C%20risas%20y%20mucho%20baile!%20Nos%20vemos%20prontito%20para%20festejar%20juntos.%0A%0A%C2%A1Un%20beso%20enorme!%0A%0AAsistiremos%3A%0A%5BEscribir%20los%20nombres%20y%20apellidos%20de%20quienes%20asistir%C3%A1n%5D"
+					href="https://wa.me/5491138978533?text=%C2%A1Hola%2C%20Eve!%0A%0AMe%20enter%C3%A9%20del%20cambio%20de%20fecha%20y%20lugar%20para%20tu%20fiesta%2C%20%C2%A1y%20quer%C3%ADa%20confirmarte%20que%20seguimos%20s%C3%BAper%20emocionad%40s%20de%20festejar%20con%20vos%20este%20momento%20tan%20especial!%0A%0A%C2%A1Por%20supuesto%20que%20confirmamos%20nuevamente%20nuestra%20asistencia!%20Va%20a%20ser%20una%20noche%20incre%C3%ADble%2C%20llena%20de%20alegr%C3%ADa%2C%20buena%20energ%C3%ADa%20y%20mucho%20baile%2C%20como%20te%20merec%C3%A9s.%0A%0A%C2%A1Nos%20vemos%20muy%20prontito%20para%20celebrar%20juntos%20como%20te%20merec%C3%A9s!%0A%0AAsistiremos%3A%0A%5BEscribir%20los%20nombres%20y%20apellidos%20de%20quienes%20asistir%C3%A1n%5D"
 					target="_blank"
 					onClick={apagarMusica}
 				>
@@ -82,7 +83,7 @@ export const Asistencia = ({ audioRef }) => {
 				</a>
 				<a
 					className="containerbtnAsistencia animate-item btn-no-asistire"
-					href="https://wa.me/5491138978533?text=Hola%2C%20Eve!%0A%0AQuería%20agradecerte%20por%20la%20invitación%20a%20tu%20fiesta%20de%2015.%20Sé%20que%20será%20una%20noche%20increíble%2C%20pero%20lamentablemente%20no%20voy%20a%20poder%20asistir.%0A%0AMe%20da%20mucha%20pena%20no%20poder%20estar%20ahí%20para%20festejar%20con%20vos%2C%20pero%20desde%20ya%20te%20deseo%20una%20noche%20inolvidable%2C%20llena%20de%20alegría%20y%20momentos%20especiales.%20%C2%A1Espero%20verte%20pronto%20para%20celebrar%20de%20otra%20manera!%0A%0ATe%20mando%20un%20abrazo%20grande%20y%20los%20mejores%20deseos%20en%20este%20día%20tan%20importante."
+					href="https://wa.me/5491138978533?text=Hola%2C%20Eve!%0A%0AQuería%20agradecerte%20nuevamente%20por%20la%20invitación%20a%20tu%20fiesta%20de%2015.%20Sé%20que%20será%20una%20noche%20increíble%2C%20pero%20lamentablemente%20con%20la%20nueva%20fecha%20no%20voy%20a%20poder%20asistir.%0A%0AMe%20da%20mucha%20pena%20no%20poder%20estar%20ahí%20para%20festejar%20con%20vos%2C%20pero%20desde%20ya%20te%20deseo%20una%20noche%20inolvidable%2C%20llena%20de%20alegría%20y%20momentos%20especiales.%20%C2%A1Espero%20verte%20pronto%20para%20celebrar%20de%20otra%20manera!%0A%0ATe%20mando%20un%20abrazo%20grande%20y%20los%20mejores%20deseos%20en%20este%20día%20tan%20importante."
 					target="_blank"
 					onClick={apagarMusica}
 				>
